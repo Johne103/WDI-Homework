551a1b3c5020766b71f92fb8a00c19ab1b77498a
John Evans <johne103@hotmail.com>
2016-10-19 08:14:47 +0100

    Update of Partials

--- johne103/w04d02/quotes-app/data/quotes.js
+++ johne103/w04d02/quotes-app/data/quotes.js
@@ -1,6 +1,6 @@
 let quotes = [
-  { id: 0, name: "Hillary Clinton", quickQuotes: "‘We should remember that just as a positive outlook on life can promote good health, so can everyday acts of kindness." },
-  { id: 1, name: "Hillary Clinton", quickQuotes: "‘Every moment wasted looking back, keeps us from moving forward… In this world and the world of tomorrow, we must go forward together or not at all." },
+  { id: 0, name: "Sarah Palin", quickQuotes: "‘I love those hockey moms. You know they say the difference between a hockey mom and a pit bull is? Lipstick" },
+  { id: 1, name: "Hillary Clinton", quickQuotes: "‘We have a lot of kids who do not know what works means. They think work is a four-letter word" },
   { id: 2, name: "Donald Trump", quickQuotes: "All of the women on The Apprentice flirted with me - consciously or unconsciously. That's to be expected." },
   { id: 3, name: "Donald Trump", quickQuotes: "What separates the winners from the losers is how a person reacts to each new twist of fate." },
   { id: 4, name: "Donald Trump", quickQuotes: "Sometimes your best investments are the ones you don't make." }
