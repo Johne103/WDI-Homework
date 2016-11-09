0c194a035058c1b32e31521d1719675cba6b250e
John Evans <johne103@hotmail.com>
2016-11-09 19:41:20 +0000

    Criminal Form with REST functions

---
+++ johne103/w07d03/criminals-app/public/js/criminalsController.js
@@ -0,0 +1,44 @@
+angular.module('criminalsApp')
+  .controller('CriminalsController', CriminalsController);
+
+CriminalsController.$inject = ['$http'];
+function CriminalsController($http) {
+  const criminals = this;
+
+  criminals.all = [];
+  criminals.addCriminal = addCriminal;
+  criminals.deleteCriminal = deleteCriminal;
+  criminals.newCriminal = {};
+
+  function addCriminal() {
+    console.log('Add criminal fired');
+    $http.post('/criminals', criminals.newCriminal)
+      .then((res) => {
+        console.log(res.data);
+        criminals.all.push(res.data);
+        criminals.newCriminal = {};
+      })
+    .catch(() => {
+      console.log('Something went wrong!');
+    });
+  }
+
+  function getCriminals() {
+    console.log('Get criminals');
+    $http.get('/criminals')
+      .then((res) => {
+        criminals.all = res.data;
+        console.log(res.data);
+      });
+  }
+
+  function deleteCriminal(id, $index) {
+    $http.delete(`/criminals/${id}`)
+      .then(() => {
+        criminals.all.splice($index, 1);
+      });
+  }
+
+
+  getCriminals();
+}
